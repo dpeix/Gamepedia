098acd35d2ad481e2e6c028f0ccb9ea2741ee68e
dpeix <dylanpeix@gmail.com>
2025-07-16 13:35:42 +0200

    Pagination

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -6,25 +6,42 @@ use App\Repository\GameRepository;
 use App\Repository\GenreRepository;
 use App\Repository\PlatformRepository;
 use App\Repository\DeveloperRepository;
+use Doctrine\ORM\Tools\Pagination\Paginator;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class HomeController extends AbstractController
 {
-    #[Route('/home', name: 'app_home')]
+    #[Route('/', name: 'app_home')]
     public function index(
         GameRepository $gameRepository,
         GenreRepository $genreRepository,
         PlatformRepository $platformRepository,
-        DeveloperRepository $developerRepository
+        DeveloperRepository $developerRepository,
+        Request $request
     ): Response {
+        $page = max(1, (int)$request->query->get('page', 1));
+        $limit = 8;
+        $offset = ($page - 1) * $limit;
+
+        $qb = $gameRepository->createQueryBuilder('g')
+            ->setFirstResult($offset)
+            ->setMaxResults($limit);
+
+        $paginator = new Paginator($qb);
+        $totalGames = count($paginator);
+        $totalPages = (int)ceil($totalGames / $limit);
+
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
-            'games' => $gameRepository->findAll(),
+            'games' => $paginator,
             'genres' => $genreRepository->findAll(),
             'platforms' => $platformRepository->findAll(),
             'developers' => $developerRepository->findAll(),
+            'currentPage' => $page,
+            'totalPages' => $totalPages,
         ]);
     }
 }
